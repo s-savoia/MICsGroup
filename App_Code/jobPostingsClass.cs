3104e18291f793aab1275eb4a84eb0ba7e141097
j7hong <j7hong@gmail.com>
2014-03-05 22:59:51 -0500

    Feature 1 - Public - Job Postings (in progress)
    - deleted old dbml file (it was using vb)
    - created new dbml file (using c#)
    - added a bin folder with AjaxControlToolkit files
    - started working on feature 1 - job postings (public)
    - created jobPostingsClass (linq class for feature 1)

---
+++ App_Code/jobPostingsClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;   
+using System.Web;
+
+
+public class jobPostingsClass
+{
+    public IQueryable<mic_job_posting> getJobPostings()
+    {
+        hospitalDataContext objHospital = new hospitalDataContext();
+        var allJobPostings = objHospital.mic_job_postings.Select(x => x);  
+        return allJobPostings;
+    }
+
+    public IQueryable<mic_job_posting> getJobPostingById(int _id)
+    {
+        hospitalDataContext objHospital = new hospitalDataContext();
+        var allJobPostings = objHospital.mic_job_postings.Where(x => x.Id == _id).Select(x => x);
+        return allJobPostings;
+    }
+
+    //public IQueryable<product> getProductByName(string _name)
+    //{
+    //    productsDataContext objProd = new productsDataContext();
+    //    var allProducts = objProd.products.Where(x => x.Name == _name).Select(x => x);
+    //    return allProducts;
+    //}
+
+    //public bool commitInsert(string _postion, string _location, string _link)   //  INSERT
+    //{
+    //    hospitalDataContext objJobPosting = new hospitalDataContext();
+    //    using (objJobPosting)
+    //    {
+    //        mic_job_posting objNewJobPosting = new mic_job_posting();
+    //        objNewJobPosting.position = _postion;
+    //        objNewJobPosting.location = _location;
+    //        objNewJobPosting.link = _link;
+    //        objNewJobPosting.mic_job_postings.InsertOnSubmit(objNewJobPosting);
+    //        objNewJobPosting.SubmitChanges();  // will commit change to database
+    //        return true;
+    //    }
+    //}
+
+    //public bool commitUpdate(int _id, string _name, string _description, decimal _price)
+    //{
+    //    productsDataContext objProd = new productsDataContext();
+    //    using (objProd)
+    //    {
+    //        var objUpProd = objProd.products.Single(x => x.Id == _id);
+    //        objUpProd.Name = _name;
+    //        objUpProd.Description = _description;
+    //        objUpProd.Price = _price;
+    //        objProd.SubmitChanges();
+    //        return true;
+    //    }
+    //}
+
+    //public bool commitDelete(int _id)
+    //{
+    //    productsDataContext objProd = new productsDataContext();
+    //    using(objProd)
+    //    {
+    //        var objDelProd = objProd.products.Single(x => x.Id == _id);
+    //        objProd.products.DeleteOnSubmit(objDelProd);
+    //        objProd.SubmitChanges();
+    //        return true;
+    //    }
+    //}
+}
---
+++ careers1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    jobPostingsClass objJobPosting = new jobPostingsClass();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        _subRebind();
+    }
+
+    private void _subRebind()
+    {        
+        rpt_data.DataSource = objJobPosting.getJobPostings();
+        rpt_data.DataBind();
+
+    }
+}
