f2e286fc6afa94580685ab821205818fd423953d
claudiareinoza <claudia.reinoza@gmail.com>
2014-03-18 18:18:59 -0400

    Slider admin done

---
+++ admin/admin_slideshow.aspx
@@ -0,0 +1,132 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/admin/admin_master.master" AutoEventWireup="true" CodeFile="admin_slideshow.aspx.cs" Inherits="_admin_slideshow" %>
+<%--****** CODE BY CLAUDIA  ******--%>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="content_main" runat="Server">
+
+        <link href="../App_Themes/admin_theme/admin_img_styles.css" rel="stylesheet" />
+
+<%--*******************--%>
+
+
+    <div class="wraper1">
+         <h1>
+                <asp:Label ID="admin1" runat="server">Admin page for the Slide-Show</asp:Label>
+         </h1>
+
+
+        <h3>
+            <asp:Label ID="lbl_message" runat="server" ForeColor="#ff8149" /></h3>
+
+
+        <h2>
+            <asp:Label ID="insert" runat="server" Text="Insert new Images:" />
+        </h2>
+        <asp:Label ID="salida" runat="server" ForeColor="Red" />
+        <br />
+        <br />
+
+        <asp:Label ID="lbl_image" AssociatedControlID="up_Image" runat="server" />
+        <asp:FileUpload ID="up_Image" runat="server" /><br />
+        <asp:Button ID="btn_add" runat="server" Text="Add image" OnClick="AddImage" />
+        
+
+        <asp:Panel runat="server" ID="pnl_datalist" Visible="false">
+            
+        
+        <asp:DataList ID="dtl_main" runat="server" DataKeyField="Id" OnEditCommand="subShowEditTemplate"
+            OnUpdateCommand="subUpdate" OnDeleteCommand="subDelete" OnCancelCommand="subCancel"
+            RepeatColumns="1" GridLines="Horizontal">
+
+            <%--template to insert the new info into the DB--%>
+            <HeaderTemplate>
+
+                <br />
+                <asp:Label ID="lbl_nameI" Text="Image name:" runat="server" AssociatedControlID="txt_nameI" />
+                <asp:TextBox ID="txt_nameI" runat="server" />
+                <asp:RequiredFieldValidator ID="rfv_name" runat="server" Text="*Required"
+                    ControlToValidate="txt_nameI" ValidationGroup="insert" Display="Dynamic" />
+                <br />
+                <asp:Label ID="lbl_link" Text="Link with:" runat="server" AssociatedControlID="txt_linkI" />
+                <asp:TextBox ID="txt_linkI" runat="server" />
+                <asp:RequiredFieldValidator ID="rfv_link" runat="server" Text="*Required"
+                    ControlToValidate="txt_linkI" ValidationGroup="insert" Display="Dynamic" />
+
+                <br />
+                <br />
+
+<%--                <asp:Label ID="lbl_image" Text="Image file:" AssociatedControlID="up_Image" runat="server" />
+        <asp:FileUpload ID="up_Image" runat="server" />
+                <br />
+                <br />--%>
+
+
+                <asp:Button ID="btn_insert" Text="INSERT" runat="server" OnClientClick="addImage" OnClick="subInsert" ValidationGroup="insert" />
+                <br />
+                <br />
+                <hr />
+            </HeaderTemplate>
+
+            <SeparatorTemplate>
+                <hr />
+            </SeparatorTemplate>
+
+
+            <%-- template to show the DB info--%>
+            <ItemTemplate>
+                <%--<asp:Label ID="lbl_id" Text='<%#Eval("Id") %>' runat="server" />--%>
+
+                <asp:Image ID="img_displayed" runat="server" ImageUrl='<%#Eval("img")%>' Width="350"/>
+
+                <br />
+                <asp:Label ID="lbl_name" Text='<%#Eval("name") %>' runat="server" />
+                <br />
+                <asp:Label ID="lbl_link" Text='<%#Eval("link") %>' runat="server" />
+                <br />
+                 <asp:Label ID="Label2" Text='<%#Eval("img") %>' runat="server" />
+                
+                <br />
+                <asp:Button ID="btn_edit" Text="EDIT" runat="server" CommandName="Edit" />
+
+            </ItemTemplate>
+
+            <%--template to edit the info--%>
+            <EditItemTemplate>
+                <h2><asp:Label ID="Label1" Text="Edit images" runat="server" /></h2>
+                <br />
+                <asp:HiddenField ID="hdf_id" Value='<%#Eval("Id") %>' runat="server" />
+                <asp:Label ID="lbl_nameE" Text="Name" runat="server" AssociatedControlID="txt_nameE" />
+                <asp:TextBox ID="txt_nameE" Text='<%#Bind("name") %>' runat="server" />
+
+                <asp:RequiredFieldValidator ID="rfv_name" runat="server" Text="*Required"
+                    ControlToValidate="txt_nameE" ValidationGroup="edit" Display="Dynamic" />
+
+                <br />
+                <asp:Label ID="lbl_linkE" Text="Link" runat="server" AssociatedControlID="txt_linkE" />
+                <asp:TextBox ID="txt_linkE" Text='<%#Bind("link") %>' runat="server" />
+                <asp:RequiredFieldValidator ID="rfv_link" runat="server" Text="*Required"
+                    ControlToValidate="txt_linkE" ValidationGroup="edit" Display="Dynamic" />
+
+                <br />
+                <br />
+
+                <asp:Button ID="btn_update" Text="UPDATE" CommandName="Update"
+                    CommandArgument='<%#Eval("Id") %>' runat="server" ValidationGroup="edit" />
+
+                <asp:Button ID="btn_delete" Text="DELETE" CommandName="Delete"
+                    CommandArgument='<%#Eval("Id") %>' runat="server" CausesValidation="false" OnClientClick="return confirm('Confirm Delete?')" />
+
+                <asp:Button ID="btn_cancel" Text="CANCEL" CommandName="Cancel" runat="server" CausesValidation="false" />
+                <br />
+                <br />
+            </EditItemTemplate>
+
+        </asp:DataList>
+
+            </asp:Panel>
+    </div>
+
+</asp:Content>
+
---
+++ admin/admin_slideshow.aspx.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using System.Object;
+using System.IO;
+//using System.IO.DirectoryInfo;
+//using System.MarshalByRefObject;
+//using System.IO.FileSystemInfo;
+
+public partial class _admin_slideshow : System.Web.UI.Page
+{
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            _subReset();
+        }
+    }
+
+    private void _subReset()
+    {
+        imagesLinkClass objLinq = new imagesLinkClass(); //creating an object from the linqClass
+
+
+
+        dtl_main.DataSource = objLinq.getImages();
+        dtl_main.DataBind(); //binding the DB
+
+    }
+
+    protected void subInsert(object sender, EventArgs e)
+    {
+
+        imagesLinkClass objLinq = new imagesLinkClass(); //creating an object from the imagesLinkClass
+
+        Button btn_insert = (Button)sender;
+        DataListItem dli = (DataListItem)btn_insert.NamingContainer;
+
+        TextBox txtNameI = (TextBox)dli.FindControl("txt_nameI");  //Reading the user imput
+        TextBox txtlinkI = (TextBox)dli.FindControl("txt_linkI");  //Reading the user imput
+
+        string img_path = salida.Text;
+
+        //_strMessage(objLinq.commitInsert(img_path, txtNameI.Text, txtlinkI.Text)), "insert");
+
+        objLinq.commitInsert(img_path, txtNameI.Text, txtlinkI.Text);
+
+        dtl_main.DataSource = objLinq.getImages();
+        dtl_main.DataBind();  //binding the DB
+    }
+
+    protected void subShowEditTemplate(object sender, DataListCommandEventArgs e)
+    {
+
+        imagesLinkClass objLinq = new imagesLinkClass(); //creating an object from the imagesLinkClass
+
+        dtl_main.DataSource = objLinq.getImages();
+        dtl_main.EditItemIndex = e.Item.ItemIndex;
+        dtl_main.DataBind();   //binding the DB
+    }
+
+    protected void subUpdate(object sender, DataListCommandEventArgs e)
+    {
+        HiddenField hdfID = (HiddenField)e.Item.FindControl("hdf_id");
+
+
+        TextBox txtNameE = (TextBox)e.Item.FindControl("txt_nameE"); //Reading the user imput
+        TextBox txtlinkE = (TextBox)e.Item.FindControl("txt_linkE");  //Reading the user imput
+
+
+        imagesLinkClass objLinq = new imagesLinkClass(); //creating an object from the imagesLinkClass
+
+        string img_path = salida.Text;
+
+        // commiting the change and passing the to parameters into _strMesage
+        _strMessage(objLinq.commitUpdate(int.Parse(hdfID.Value.ToString()), txtNameE.Text, txtlinkE.Text, img_path), "update");
+
+        dtl_main.DataSource = objLinq.getImages();
+        dtl_main.EditItemIndex = -1;
+        dtl_main.DataBind(); //binding the DB
+        _subReset();// reseting textboxes
+    }
+
+    protected void subDelete(object sender, DataListCommandEventArgs e)
+    {
+
+        imagesLinkClass objLinq = new imagesLinkClass();  //creating an object from the imagesLinkClass
+
+
+        HiddenField hdfID_D = (HiddenField)e.Item.FindControl("hdf_id"); // reading the id value
+
+        int imgID_D = int.Parse(hdfID_D.Value.ToString()); //converting the id in number
+        //int x = Convert.ToInt32(hdfID_D);
+
+        _strMessage(objLinq.commitDelete(imgID_D), "delete"); // commiting the change and passing the to parameters into _strMesage
+
+
+        dtl_main.DataSource = objLinq.getImages();
+        dtl_main.EditItemIndex = -1;// to go back and hide de edit panel
+        dtl_main.DataBind();//binding the DB
+        _subReset();
+
+    }
+
+    protected void subCancel(object sender, DataListCommandEventArgs e)
+    {
+
+        imagesLinkClass objLinq = new imagesLinkClass(); //creating an object from the imagesLinkClass
+
+        dtl_main.DataSource = objLinq.getImages();
+        dtl_main.EditItemIndex = -1;
+        dtl_main.DataBind();
+
+        //frm_img.DataSource = objLinq.getImages();
+        //frm_img.DataBind();
+    }
+
+    private void _strMessage(bool flag, string str)
+    {
+        if (flag) // if flag is true then was successful
+            lbl_message.Text = "Image " + str + " was successful"; //writing the output
+        else
+            lbl_message.Text = "Sorry, unable to" + str + " Image";
+    }
+
+    //************* INSERTING IMAGE WITH FILEUPLOAD ****************//
+
+    public void AddImage(object sender, EventArgs e)
+    {
+        if (up_Image.HasFile)
+        {
+            if (CheckFile(up_Image.FileName))
+            {
+                String filePath = "~/img1/" + up_Image.FileName;
+                up_Image.SaveAs(MapPath(filePath));
+
+                salida.Text =filePath;
+
+                pnl_datalist.Visible = true;/// para ver el panel
+                                            /// 
+                //string path_image = filePath; COMO SACAR ESTE VALOR DE AQUI PARA ASIGNARLO AL IMG_PATH DE ARRIBA
+
+                return;
+
+                
+
+            }
+        }
+    }
+
+    bool CheckFile(string fileName)
+    {
+        string ext = Path.GetExtension(fileName);
+
+        switch (ext.ToLower())
+        {
+            case ".gif":
+                return true;
+            case ".jpg":
+                return true;
+            case ".png":
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    //private void _panelControl(Panel pnl)
+    //{
+    //    pnl_datalist.Visible = false;
+    //    pnl.Visible = true;
+    //}
+
+}
+
+    //void Page_PreRender()
+    //{
+    //    string upFolder = MapPath("~/img1/");
+    //    DirectoryInfo dir=new DirectoryInfo(upFolder);
+
+    //    }
+    
+    
+
+
+
+
+    //protected void UpLoadImg(object sender, FormViewInsertEventArgs e)
+    //{
+    //    FileUpload upImage = (FileUpload)frm_img.FindControl("up_img");
+
+    //    frm_img.
+    
+    //}
+
+
+    
